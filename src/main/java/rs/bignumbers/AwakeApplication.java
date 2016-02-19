4d94a968cf93641e4aa04ec6efc37d6fd85b0751
zeljko.gavrilovic <zeljko.m.gavrilovic@gmail.com>
2016-02-19 11:13:26 +0100

    added spring boot jdbc support

---
+++ src/main/java/rs/bignumbers/AwakeApplication.java
@@ -0,0 +1,44 @@
+package rs.bignumbers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import rs.bignumbers.model.Person;
+import rs.bignumbers.service.PersonService;
+
+@SpringBootApplication
+public class AwakeApplication implements CommandLineRunner {
+
+	Logger logger = LoggerFactory.getLogger(AwakeApplication.class);
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	private PersonService personService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwakeApplication.class, args);
+	}
+
+	public void run(String... args) {
+		Person person = new Person();
+		person.setFirstName("FName");
+		person.setLastName("LName");
+		person.setAge(20);
+		person.setPlace("Place");
+
+		if (personService.addPerson(person) > 0) {
+			logger.info("Person saved successfully");
+		}
+
+		for (Person p : personService.getAllPerson()) {
+			logger.info(p.toString());
+		}
+	}
+}
